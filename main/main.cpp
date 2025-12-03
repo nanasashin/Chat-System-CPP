c1af31d9a22b45da6cdfbac4c8deabf753065373
NatsuShii <renzjoshuamalig10@gmail.com>
2025-12-03 16:38:41 +0800

    optimize chat and functions

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <map>
 
 const int USER_LIMIT = 100;
 
 struct Chat {
-    std::string receiver_username;
+    bool exist = false;
     std::string user_chat[100];
     std::string receiver_chat[100];
     int chat_handler[200];
@@ -14,8 +15,7 @@ struct User {
     std::string username;
     std::string password;
     bool is_empty = true;
-    std::string friends[100];
-    Chat chat[100];
+    std::map<std::string, Chat> messages;
 };
 
 User user[USER_LIMIT];
@@ -71,54 +71,51 @@ class UI : public Utils {
 public: 
     User current_user;
 private:
-    bool login() {
-        print_line();
-
-        string username = get_string("Username");
-
+    bool login(string username, string password) {
         if (!user_exist(username)) {
             cout << "User does not exist!!\n";
             return false;
         }
-
-        string password = get_string("Password");
+        
         bool can_log_in = check_password(username, password);
 
         if (!can_log_in) {
             cout << "Incorrect Password!!\n";
             return false;
         }
-        
-        cout << "Logging In Username: " << username << endl;
-        update_current_user(user_return("username"));
 
-        print_line();
-
+        cout << "Logging in: " << username << endl;
         return true;
     }
 
-    bool sign_up() {
-        print_line();
-
-        string username = get_string("Username");
-        
+    bool sign_up(string username, string password) {
         if (user_exist(username)) {
             cout << "Username Unavailable\n";
             return false;
         }
 
-        string password = get_string("Password");
+        User the_user = user_return(username);
+        the_user.password = password;
 
-        update_current_user(user_return(username));
+        update_current_user(the_user);
         update_database_user(username);
         return true;
     }
 
-    bool add_friend() {
-        
+    bool add_friend(string username) {
+        if (current_user.messages[username].exist) {
+            cout << "User is already a friend\n";
+            return false;
+        }
+        current_user.messages[username].exist = true;
+        return true;
     }
 
-    User update_current_user(User the_user) {
+    bool remove_friend(string username) {
+        current_user.messages.erase(username);
+    }
+
+    void update_current_user(User the_user) {
         current_user = the_user;
     }
 
